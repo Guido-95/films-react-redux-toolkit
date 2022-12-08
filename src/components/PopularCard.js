a58e0674738a79fd8f14152e33edafad1700800e
nate12341 <96428199+nate12341@users.noreply.github.com>
2022-12-08 14:37:56 +0100

    fix

--- src/components/PopularCard.js
+++ src/components/PopularCard.js
@@ -17,7 +17,6 @@ function PopularCard({poster_path,title,vote_average,id,media_type,name}) {
   return (
     
     <Link to={`/${media_type}/${id}`} className='popular-card'>
-      {console.log(arrotondaPercentuale(vote_average))}
         <div className='img-container'>
           <img className='img-popular-card' src={`https://image.tmdb.org/t/p/w300/${poster_path}`} alt="" />
           <div className={`percentual ${ arrotondaPercentuale(vote_average) >= 75 ? 'percentual-green' : arrotondaPercentuale(vote_average) >= 50 ? 'percentual-yellow' : arrotondaPercentuale(vote_average) === "NV" ? 'no-percentual' : 'percentual-red' } `}>{arrotondaPercentuale(vote_average)}</div> 
