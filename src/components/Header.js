ee1f1c940aea7b020df5a7d1d151c5b0fa8479a5
nate12341 <96428199+nate12341@users.noreply.github.com>
2023-02-28 17:45:09 +0100

    fix logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -38,7 +38,7 @@ function Header() {
           <Link to="/" className='logo-link' onClick={resetPage}>
             <img className='logo' alt='logo' src={logo} />
             <div>
-              React Films
+              React Movies
             </div>
           
           </Link>
